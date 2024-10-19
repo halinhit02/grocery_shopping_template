9a040b1a2cd97748fb5bda80c022a2d019d2ab9b
HALINHIT <linhhaofficial@gmail.com>
2024-10-19 21:23:38 +0700

    update

--- android/app/src/main/kotlin/com/example/grocery_shopping_template/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/grocery_shopping_template/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.grocery_shopping_template
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/grocery_shoppingapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.grocery_shoppingapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
